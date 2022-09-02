5efc3ff37af03bc737756b9d6a97505e949846ea
vishymails <vishymails@gmail.com>
2022-09-02 13:10:24 +0530

    added

--- first-example/src/app/app.module.ts
+++ first-example/src/app/app.module.ts
@@ -5,16 +5,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import  { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { CustomerComponent } from './customer/customer.component';
+import { SupplierComponent } from './supplier/supplier.component';
+import { AssociateComponent } from './associate/associate.component';
+import { LoginModule } from './login/login.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent
+    BooksComponent,
+    ServerComponent,
+    ServersComponent,
+    Comp2Component,
+    CustomerComponent,
+    SupplierComponent,
+    AssociateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ first-example/src/app/supplier/supplier.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier',
+  template: `
+    <p>
+      supplier works!
+    </p>
+  `,
+  styleUrls: ['./supplier.component.css']
+})
+export class SupplierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
