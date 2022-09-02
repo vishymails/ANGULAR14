c5178c6fba91da70fccd876a8350d699f7ffa108
vishymails <vishymails@gmail.com>
2022-09-02 10:50:25 +0530

    added

--- first-example/src/app/books/books.component.ts
+++ first-example/src/app/books/books.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
+  name : string = "Sherlock Holmes Series Complete ";
+
+  author : string = " Conan Doyle";
+
+  src :string = 'https://images-na.ssl-images-amazon.com/images/I/51rvC3FJqkL._SX451_BO1,204,203,200_.jpg';
+
+  name2 : string = "Siddhartha  ";
+
+  author2 : string = " Hermann Hesse";
+
+  src2 :string = 'https://images-na.ssl-images-amazon.com/images/I/41QPBtd5VIS._SX460_BO1,204,203,200_.jpg';
+
+
   constructor() { }
 
   ngOnInit(): void {
