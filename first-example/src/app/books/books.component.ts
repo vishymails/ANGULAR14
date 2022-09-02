aa07041b63732756c665e808ee8ffeabf75253d7
vishymails <vishymails@gmail.com>
2022-09-02 17:28:42 +0530

    added

--- first-example/src/app/books/books.component.ts
+++ first-example/src/app/books/books.component.ts
@@ -5,6 +5,7 @@ interface Book {
   name : string;
   author : string;
   src : string;
+  price : number;
 
 }
 
@@ -29,13 +30,15 @@ export class BooksComponent implements OnInit {
     {
       name : "sherlock holmes series complete ",
       author : " Conan Doyle",
-      src : 'https://images-na.ssl-images-amazon.com/images/I/51rvC3FJqkL._SX451_BO1,204,203,200_.jpg'
+      src : 'https://images-na.ssl-images-amazon.com/images/I/51rvC3FJqkL._SX451_BO1,204,203,200_.jpg',
+      price : 550
 
     },
     {
       name : "siddhartha",
       author : " Hermann Hesse",
-      src : 'https://images-na.ssl-images-amazon.com/images/I/41QPBtd5VIS._SX460_BO1,204,203,200_.jpg'
+      src : 'https://images-na.ssl-images-amazon.com/images/I/41QPBtd5VIS._SX460_BO1,204,203,200_.jpg',
+      price : 1200
 
     }
   ]
