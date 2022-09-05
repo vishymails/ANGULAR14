47e882bee02a25891f6d518d1ddaa1f901ceb025
vishymails <vishymails@gmail.com>
2022-09-05 11:05:48 +0530

    added

--- first-example/src/app/books/books.component.ts
+++ first-example/src/app/books/books.component.ts
@@ -26,6 +26,19 @@ export class BooksComponent implements OnInit {
 
   companies = ["ISRO", "Oracle", "Microsoft", "CitiBank", "ST Electronics", "IBM", "Cyient"];
 
+  
+  data : string ="Dr.Rao";
+
+ 
+  isShowing : boolean = true;
+
+
+  customText : string = " Wikimedia Foundation, Inc. is a 501 nonprofit organization headquartered in San Francisco, California, registered as a charitable foundation under US law. It owns the Wikimedia projects – which are written and curated by thousands of Wikipedians and other unpaid volunteers";
+
+  file = { name : "bvr.svg", size : 21200190, type : 'image/svg'};
+
+
+
   books : Book[] = [
     {
       name : "sherlock holmes series complete ",
@@ -40,23 +53,33 @@ export class BooksComponent implements OnInit {
       src : 'https://images-na.ssl-images-amazon.com/images/I/41QPBtd5VIS._SX460_BO1,204,203,200_.jpg',
       price : 1200
 
+    },
+    {
+      name : "Rich Dad Poor Dad ",
+      author : "Robert T. Kiyosaki ",
+      src : 'https://images-na.ssl-images-amazon.com/images/I/517VIc78Z0L._SX331_BO1,204,203,200_.jpg',
+      price : 1000
+
+    },
+    {
+      name : "India, Bharat and Pakistan : The Constitutional Journey of Sandwiched Civilisation",
+      author : "J Sai Deepak ",
+      src : 'https://m.media-amazon.com/images/I/71ZKMWgafjL._AC_UY218_.jpg',
+      price : 574
+
+    },
+    {
+      name : "MILLION MINDS ENGLISH UP TGT ENGLISH 20 PRACTICE SETS",
+      author : "Yogesh Kumar",
+      src : 'https://m.media-amazon.com/images/I/717madWznGL._AC_UY218_.jpg',
+      price : 198
+
     }
   ]
 
   
   
 
-
-
-  data : string ="Dr.Rao";
-
- 
-  isShowing : boolean = true;
-
-
-  customText : string = " Wikimedia Foundation, Inc. is a 501 nonprofit organization headquartered in San Francisco, California, registered as a charitable foundation under US law. It owns the Wikimedia projects – which are written and curated by thousands of Wikipedians and other unpaid volunteers";
-
-  file = { name : "bvr.svg", size : 21200190, type : 'image/svg'};
 
   constructor() { }
 
