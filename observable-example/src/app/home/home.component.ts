89efb2175d50cdca1ab0b83119b5ea90207348f5
vishymails <vishymails@gmail.com>
2022-09-07 12:27:21 +0530

    added

--- observable-example/src/app/home/home.component.ts
+++ observable-example/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -18,9 +18,34 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.firstObsSubcription = interval(1000).subscribe(count => {
-      console.log(count);
-    })
+    // this.firstObsSubcription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+
+    const customIntervalObservable = Observable.create((observer: { next: (arg0: number) => void; complete: () => void; error: (arg0: Error) => void; }) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if(count === 5 ) {
+          observer.complete();
+        }
+        if(count > 3) {
+          observer.error(new Error('Count is greater than 3 '));
+        }
+        count++;
+
+      }, 1000);
+    });
+
+
+    this.firstObsSubcription = customIntervalObservable.subscribe((data: any) => {
+      console.log(data);
+    }, (error: { message: any; }) => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log("completed");
+    });
   }
 
 }
